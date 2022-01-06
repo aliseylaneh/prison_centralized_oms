5c63b40e1b65cfba7d26e3c72f4e9b87b4f1cc57
ali seylaneh <aliseylaneh@gmail.com>
2022-01-06 05:06:29 +0330

    report page is only available for administrator

--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
 # Create your views here.
+from account.decorators import allowed_users
 
 
+@login_required(login_url='account:login')
+@allowed_users(['administrator'])
 def report_home(request):
     return render(request, 'reports/home.html')
