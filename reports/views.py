708f2201398cc33196c24742798d1411bd575046
ali seylaneh <aliseylaneh@gmail.com>
2022-01-09 06:32:49 +0330

    sell price and buy price is fixed for category report

--- reports/views.py
+++ reports/views.py
@@ -147,8 +147,14 @@ def review_request(request):
                     sellprice = 0
                     buyprice = 0
             except SupplierProduct.DoesNotExist:
-                sellprice = 0
-                buyprice = 0
+                supplier_product_price = SupplierProduct.objects.filter(product=order.product,
+                                                                        supplier=order.supplier)
+                if len(supplier_product_price) != 0:
+                    sellprice = supplier_product_price[0].price2m
+                    buyprice = supplier_product_price[0].price
+                else:
+                    sellprice = 0
+                    buyprice = 0
             request_orders.append({'product_name': order.product.name, 'product_category': order.product.category.name,
                                    'product_supplier': order.supplier.company_name,
                                    'product_brand': order.brand.company_name, 'product_quantity': order.quantity,
